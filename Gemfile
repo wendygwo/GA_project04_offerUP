d004d74140ec85372a8867e40702a73aa3913f1c
SeaOfLee <l.richardson1@gmail.com>
2015-03-21 12:28:33 -0700

    Add geocoder gem and lat/long save to new User

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'figaro'
 gem 'bcrypt'
 gem 'aws-sdk'
-
+gem 'geocoder'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 require 'bcrypt'
 class User < ActiveRecord::Base
 	has_secure_password
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+     "#{city}, #{state}, #{zip_code}"
+  end
 end
