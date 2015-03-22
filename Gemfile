d6b6453830c306239a9d861ab1ae7295dfdd2df7
SeaOfLee <l.richardson1@gmail.com>
2015-03-21 18:15:24 -0700

    Add Faker gem and dummy data to seeds.rb

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'faker', git: "https://github.com/stympy/faker.git"
 gem 'aws-sdk', '< 2.0'
 gem "paperclip", "~> 4.2"
 gem 'figaro'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do
+  User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    city: Faker::Address.city,
+    state: Faker::Address.state,
+    zip_code: Faker::Address.zip,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude
+    )
+end
+
+20.times do
+  Good.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraphs(2),
+    user_id: Faker::Number.number(2)
+    )
+end
