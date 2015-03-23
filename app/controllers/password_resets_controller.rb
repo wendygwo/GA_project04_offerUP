db6e3dd4c0a421d95d5ac78371fbf4a5d8a8fffb
SeaOfLee <l.richardson1@gmail.com>
2015-03-22 20:49:31 -0700

    Fix redirect error on password_reset#create

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,7 +6,7 @@ class PasswordResetsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	user.send_password_reset if user
   	flash[:notice] = 'E-mail sent with password reset instructions.'
-  	redirect_to new_session_path
+  	redirect_to new_sessions_path
   end
 
   def edit
