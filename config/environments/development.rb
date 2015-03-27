464a152879c71403279817ff5d5175adfe0f71b1
Wendy Gwo <wgwo@hotmail.com>
2015-03-26 20:33:35 -0700

    Add gmail mailer to development and production environment files.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,18 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # config letter_opener gem
-  config.action_mailer.delivery_method = :letter_opener
+  # config.action_mailer.delivery_method = :letter_opener
+  #Relay e-mail through gmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            ENV['GMAIL_USERNAME'],
+  password:             ENV['GMAIL_PASSWORD'],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
+
   # Configure default URL for action mailer
   config.action_mailer.default_url_options = {:host =>'localhost:3000'}
   
